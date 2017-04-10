619837f911ed4910535e8296124a85760564bdf1
Alasdair Collinson <blalasaadri@googlemail.com>
2017-04-10 10:24:22 +0200

    Added guards

--- elixir-and-elm/temporary/lib/temporary.ex
+++ elixir-and-elm/temporary/lib/temporary.ex
@@ -8,9 +8,21 @@ defmodule Temporary do
 
   ## Examples
 
+      iex> Temporary.add(0, 0)
+      0
+
+      iex> Temporary.add(0, 2)
+      2
+
+      iex> Temporary.add(3, 0)
+      3
+
       iex> Temporary.add(1, 1)
       2
 
   """
-  def add(x, y), do: x + y
+  def add(0, 0), do: 0
+  def add(0, y) when is_number(y), do: y
+  def add(x, 0) when is_number(x), do: x
+  def add(x, y) when is_number(x) and is_number(y), do: x + y
 end
