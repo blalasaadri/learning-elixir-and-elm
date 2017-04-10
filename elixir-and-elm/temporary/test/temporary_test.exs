ea57d035f373535e49da849a6f2e7adb76016c4a
Alasdair Collinson <blalasaadri@googlemail.com>
2017-04-10 09:58:38 +0200

    Added more tests for adder

--- elixir-and-elm/temporary/test/temporary_test.exs
+++ elixir-and-elm/temporary/test/temporary_test.exs
@@ -2,7 +2,19 @@ defmodule TemporaryTest do
   use ExUnit.Case
   doctest Temporary
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "the add function takes two integers and adds them up" do
+    result = Temporary.add(1, 1)
+    assert result == 2
+  end
+
+  test "the add functino takes two floats and adds them up" do
+    result = Temporary.add(1.5, 2.5)
+    assert result == 4.0
+  end
+
+  test "the add function returns a number" do
+    Temporary.add(1.5, 3.5)
+    |> is_number
+    |> assert
   end
 end
