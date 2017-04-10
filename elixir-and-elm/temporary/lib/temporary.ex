c9bb43e133c21bca956c01fca20f5534f7a26e8b
Alasdair Collinson <blalasaadri@googlemail.com>
2017-04-10 09:40:18 +0200

    Added temporary project

---
+++ elixir-and-elm/temporary/lib/temporary.ex
@@ -0,0 +1,18 @@
+defmodule Temporary do
+  @moduledoc """
+  Documentation for Temporary.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Temporary.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir-and-elm/temporary/test/temporary_test.exs
@@ -0,0 +1,8 @@
+defmodule TemporaryTest do
+  use ExUnit.Case
+  doctest Temporary
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
