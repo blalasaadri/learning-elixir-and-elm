4e0b55c0831489c2003ab36c16f8384697afc962
Alasdair Collinson <blalasaadri@googlemail.com>
2017-04-10 09:46:51 +0200

    Replaced 'hello' function with an adder

--- elixir-and-elm/temporary/lib/temporary.ex
+++ elixir-and-elm/temporary/lib/temporary.ex
@@ -4,15 +4,15 @@ defmodule Temporary do
   """
 
   @doc """
-  Hello world.
+  Add up two numbers.
 
   ## Examples
 
-      iex> Temporary.hello
-      :world
+      iex> Temporary.add(1, 1)
+      2
 
   """
-  def hello do
-    :world
+  def add(x, y) do
+    x + y
   end
 end
